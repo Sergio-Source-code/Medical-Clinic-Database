051f6b4d62672855cfe60b7d33ccd034ff40a908
Sergio-Source-code <sergiosanzescallon@gmail.com>
2022-03-28 22:46:47 -0500

    Added basic files

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 </head>
 <body>
   <h1>Hello, we are a Medical Clinic.</h1>
+  <button onclick="window.location.href = 'signin.php';">Sign in</button>
+  <button onclick="window.location.href = 'signup.php';">Sign up</button>
+  <a href="patient.php">visit patient page</a>
+  <a href="doctor.php">visit doctor page</a>
+  <a href="admin.php">visit admin page</a>
 </body>
 </html>
